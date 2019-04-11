0a8730c07e2ed792859f0310149db37e642d9cf0
librarylai <library@intella.co>
2019-04-11 18:43:02 +0800

    init project

---
+++ precache-manifest.dad9f4fc2092ed4b0feeed74fb157a3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdb3be9fa932750fa2c1e32d61c6eafd",
+    "url": "./static/media/lai.bdb3be9f.JPG"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "58575d55255ed026d179",
+    "url": "./static/js/main.58575d55.chunk.js"
+  },
+  {
+    "revision": "d9790b72ef7c44c5658c",
+    "url": "./static/js/2.d9790b72.chunk.js"
+  },
+  {
+    "revision": "58575d55255ed026d179",
+    "url": "./static/css/main.cd876d98.chunk.css"
+  },
+  {
+    "revision": "14f58bb6f9f6d6bd3690d367d36a725c",
+    "url": "./index.html"
+  }
+];
